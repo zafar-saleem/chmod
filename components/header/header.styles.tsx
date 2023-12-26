ccbf6900fbf359fb82dad0b43911fefe8e7d97c4
Zafar Saleem <zafarsaleem3@gmail.com>
2023-12-26 21:10:08 +0500

    fix: fixed responsiveness

--- components/header/header.styles.tsx
+++ components/header/header.styles.tsx
@@ -4,4 +4,9 @@ export const Title = styled.h1`
   font-size: 1.5rem;
   text-align: center;
   padding: 2rem;
+
+  @media screen and (max-width: ${props => props.theme.breakpoints.s}) {
+    padding: 0 1rem;
+    text-align: left;
+  }
 `;
